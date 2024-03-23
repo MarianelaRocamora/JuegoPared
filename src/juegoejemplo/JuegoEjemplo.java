2a6cf2ba80a4f064ca1066b5ed8b59079c05e0ad
MarianelaRocamora <rocamoramarianela86@gmail.com>
2024-03-23 10:56:13 -0300

    descripcion
    comentario de inicio

--- src/juegoejemplo/JuegoEjemplo.java
+++ src/juegoejemplo/JuegoEjemplo.java
@@ -6,7 +6,7 @@ public class JuegoEjemplo {
 
    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("-------------Inicio del juego-----------");
         Auto ford = new Auto("rojo", 10,5);
         Escena escena1=new Escena(ford);
         escena1.jugar();
